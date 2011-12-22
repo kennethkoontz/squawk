4ab28a68d676aea03cc5ddbc36b0bda41b05858e
Kenneth Koontz <kenneth.koontz@gmail.com>
2011-12-22 00:07:33 -0800

    messages can now be sent back and forth

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var app = require('express').createServer()
   , express = require('express')
   , jqtpl = require('jqtpl')
   , redis = require("redis")
+  , io = require('socket.io').listen(app)
   , client = redis.createClient()
   , publisher = redis.createClient()
   , subscriber = redis.createClient();
@@ -10,10 +11,22 @@ app.set("view engine", "html");
 app.register(".html", require("jqtpl").express);
 app.use("/javascripts", express.static(__dirname + '/javascripts'));
 app.use("/styles", express.static(__dirname + '/styles'));
+app.use("/models", express.static(__dirname + '/models'));
 
 client.on("error", function (err) {
     console.log("Error " + err);
-    asdfasf
+});
+
+io.sockets.on('connection', function (socket) {
+    subscriber.subscribe('channel');
+
+    subscriber.on("message", function (channel, message) {
+        socket.send(message);
+    });
+
+    socket.on('message', function (data) {
+        publisher.publish('channel', data);
+    });
 });
 
 app.get('/', function(req, res) {
