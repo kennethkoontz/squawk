4d0e429ab0c83dfb89c38f92b2cee09c4b0b4222
kenneth koontz <kenneth.koontz@gmail.com>
2011-12-23 01:13:39 -0800

    interim commit for authentication system

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 var app = require('express').createServer()
   , express = require('express')
-  , jqtpl = require('jqtpl')
+  , jade = require('jade')
   , redis = require("redis")
+  , mongoose = require('mongoose').connect('localhost', 'squawk', '27017')
   , io = require('socket.io').listen(app)
   , client = redis.createClient()
   , publisher = redis.createClient()
-  , subscriber = redis.createClient();
+  , subscriber = redis.createClient()
+  , RedisStore = require('connect-redis')(express);
 
-app.set("view engine", "html");
-app.register(".html", require("jqtpl").express);
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId;
+
+var UserModel = new Schema({
+    id: ObjectId
+  , email: String
+  , firstName: String
+  , lastName: String
+  , password: String
+});
+
+var User = mongoose.model('UserModel', UserModel);
+
+app.set("view engine", "jade");
+app.set("view options", { layout: false });
+app.register(".jade", require("jade").express);
 app.use("/javascripts", express.static(__dirname + '/javascripts'));
 app.use("/styles", express.static(__dirname + '/styles'));
 app.use("/models", express.static(__dirname + '/models'));
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'secretpassword', store: new RedisStore}));
 
 client.on("error", function (err) {
     console.log("Error " + err);
@@ -30,10 +49,25 @@ io.sockets.on('connection', function (socket) {
 });
 
 app.get('/', function(req, res) {
-    app.set("view options", {
-        layout: false
+    res.render('layout');
+});
+
+app.post('/room', function(req, res) {
+    User.find({ email: req.body.email}, function (err, doc) {
+        if (err) {
+            console.log(err);
+        }
+        if (doc.length === 0) {
+            var user = new User(req.body);
+            user.save(function (err) {
+                // If there is an error here we should raise a 500 error.
+                console.log(err);
+            });
+            res.send('registered'+user);
+        } else {
+            res.send('user already registered');
+        }
     });
-    res.render('room');
 });
 
 app.listen(8000);
